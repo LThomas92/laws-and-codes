b1de532cafc0c1ba3440daa7cd13190447331247
Lawrence <lthomas@administrators-iMac.local>
2024-03-04 15:14:53 -0500

    updates to newsletter styles

--- wp-content/themes/laws-and-codes/page-thank-you.php
+++ wp-content/themes/laws-and-codes/page-thank-you.php
@@ -6,18 +6,20 @@
     $title = get_field('title');
     $content = get_field('content');
     $icon = get_field('icon');
+    $problemCTA = get_field('problem_cta');
   ?>
   <h1 class="c-page-thank-you__title"><?php echo $title; ?></h1>
 
   <div class="c-page-thank-you__icon">
-    <img src="" alt="Check Mark">
+    <img src="<?php echo $icon['url']; ?>" alt="Check Mark">
   </div>
 
   <div class="c-page-thank-you__content">
     <?php echo $content; ?>
   </div>
 
-  <a target="_blank" href="<?php echo site_url(); ?>">Back to Home</a>
+  <a class="c-page-thank-you__cta" target="_blank" href="<?php echo site_url(); ?>">Back to Home</a>
+  <a class="c-page-thank-you__cta-problem" href="<?php echo $problemCTA['url']; ?>"><?php echo $problemCTA['title']; ?></a>
 </div>
 
 
